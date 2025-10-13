53a5337540fc6e3892f9256c7fbd3ff897b18697
Wilhelm Macaigne <wmacaigne@gmail.com>
2025-10-13 15:23:56 +0200

    commit 2

---
+++ exo2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int C, t, n, M;
+    int taux_mensuel;
+
+    printf("Montant du prêt C");
+    scanf("%d", &C);
+
+    printf("Taux d'intérêt annuel");
+    scanf("%d", &t);
+
+    printf("Durée du prêt ");
+    scanf("%d", &n);
+
+    return 0;
+
+     
+
+}
