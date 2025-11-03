28723a869d3982ebb4bc75de392749c64dac4378
Wilhelm Macaigne <wmacaigne@gmail.com>
2025-11-03 15:09:14 +0100

    & et caracteres

--- exec6.c
+++ exec6.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    char nom[4];
+    char nom[6];// Il faut augmenter le nombre de caracteres
     printf("Nom ? ");
-    scanf("%s", nom); 
+    scanf("%s", &nom);// Il manque le &
     printf("Bonjour %s\n", nom);
     return 0;
 }
