d20a4eb345e2b9b6e48a329b016695469f12e9e3
Wilhelm Macaigne <wmacaigne@gmail.com>
2025-11-03 14:03:09 +0100

    %c bonne definition

--- compil5.c
+++ compil5.c
@@ -2,6 +2,6 @@
 
 int main() {
     char c = 'A';
-    printf("c = %s\n", c);
+    printf("c = %c\n", c);//Le %s etait l'erreur car c'etait pour une chaine de caractere alors que c est un caractere unique
     return 0;
 }
