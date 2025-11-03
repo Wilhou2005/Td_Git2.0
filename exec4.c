1eea2ce959222aa436541c062bf55b0eeaa67421
Wilhelm Macaigne <wmacaigne@gmail.com>
2025-11-03 15:05:01 +0100

    s[4]

--- exec4.c
+++ exec4.c
@@ -4,7 +4,7 @@ int main() {
     int i = 0;
     while (i < 10) {
         printf("%d\n", i);
-        i++// I n'est jamais modifie dans la boucle il faut donc mettre i++
+        i++;// I n'est jamais modifie dans la boucle il faut donc mettre i++
     }
     return 0;
 }
--- exec5.c
+++ exec5.c
@@ -6,6 +6,7 @@ int main() {
     s[1] = 'e';
     s[2] = 'y';
     s[3] = '!';
+    s[4] = '\0';//Avec cette ligne il va lire les caracteres jusqu'à 0 sans ça ça peut ne pas fonctionner
     printf("%s\n", s); 
     return 0;
 }
