6de2cdbaa95ade5093264b08557e0fa19a8ebe56
Wilhelm Macaigne <wmacaigne@gmail.com>
2025-11-03 14:06:32 +0100

    ==

--- compil6.c
+++ compil6.c
@@ -2,7 +2,7 @@
 
 int main() {
     int x = 10;
-    if (x = 5) {
+    if (x == 5) { // Il ne faut pas utuliser = mais == car il s'agit d'une comparaison
         printf("x vaut 5\n");
     }
     return 0;
