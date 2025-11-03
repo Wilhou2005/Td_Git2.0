c9cc521119addd53a3179914b7db38163b9719de
Wilhelm Macaigne <wmacaigne@gmail.com>
2025-11-03 14:54:20 +0100

    x valeur 0

--- exec3.c
+++ exec3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int x;
+    int x = 0;// x est donné sans avoir de valeur il faut donc lui en donner une 
     int y = x + 5;  
     printf("%d\n", y);
     return 0;
