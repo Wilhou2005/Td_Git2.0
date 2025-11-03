b48df353ba005bd6674f8b128628ba5ca47cd1d8
Wilhelm Macaigne <wmacaigne@gmail.com>
2025-11-03 15:12:19 +0100

    float

--- exec7.c
+++ exec7.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int distance_km = 100;
-    int vitesse_kmh = 50;
-    int temps = distance_km / vitesse_kmh; 
-    int minutes = temps * 60;
-    printf("minutes = %d\n", minutes); 
+    float distance_km = 100;//En divisant on peut avoir un nombre décimal donc on utilise float
+    float vitesse_kmh = 50;
+    float temps = distance_km / vitesse_kmh; 
+    float minutes = temps * 60;
+    printf("minutes = %f\n", minutes); //%f car on utilise float
     return 0;
 }
