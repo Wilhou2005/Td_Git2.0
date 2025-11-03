d510fdf6d6a6c69e25cd691e001eab4e67fc6e32
Wilhelm Macaigne <wmacaigne@gmail.com>
2025-11-03 14:13:39 +0100

    int y

--- compil8.c
+++ compil8.c
@@ -2,6 +2,7 @@
 
 int main() {
     int x = 10;
-    printf("x = %d\n", y);
+    int y = 5;
+    printf("x = %d\n", y);//'y' undeclared (first use in this function) y n'etait pas declarer donc ça ne pouvait pas marcher 
     return 0;
 }
