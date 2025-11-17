0149b284a3feef10c2cac76756f4e5daa0b31d4d
Wilhelm Macaigne <wmacaigne@gmail.com>
2025-11-17 13:29:22 +0100

    etape 2 plus tableau si la taille est superieur ou egal a la bvaleur

--- Tp3/tp3exo1.c
+++ Tp3/tp3exo1.c
@@ -1,30 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main() {
     int n;
-    printf("Entrez une taille pour l'etoile (entre 5 et 10) : ");
-    scanf("%d", &n);
+
+    do {
+        printf("Entrez une taille pour l'etoile (entre 5 et 10) : ");
+        scanf("%d", &n);
+    } while (n < 5 || n > 10);
 
     int width = n * n;
     int max_height = 3 * n - 1;
 
     char img[max_height][width];
 
+    for (int i = 0; i < max_height; i++) {
+        for (int j = 0; j < width; j++) {
+            img[i][j] = ' ';
+        }
+    }
+
     return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
- 
 }
