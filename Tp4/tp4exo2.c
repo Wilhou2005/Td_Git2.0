21c20f6d67db335618a6adcd381056e85cd70b9a
Wilhelm Macaigne <wmacaigne@gmail.com>
2025-11-26 22:07:51 +0100

    etape 2

--- Tp4/tp4exo2.c
+++ Tp4/tp4exo2.c
@@ -7,10 +7,22 @@ void afficher_menu() {
     printf("2 - Quitter\n");
 }
 
+int saisir_montant() {
+    int m;
+    printf("Montant a retirer : ");
+    scanf("%d",&m);
+    return m;
+}
+
 int main() {
     int choix;
     afficher_menu();
     printf("Votre choix : ");
     scanf("%d",&choix);
+
+    if (choix == 1) {
+        int m = saisir_montant();
+    }
+
     return 0;
 }
