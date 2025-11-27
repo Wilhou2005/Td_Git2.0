458f6d92e8a7dc535de90de6d3e8473586e46d15
Wilhelm Macaigne <wmacaigne@gmail.com>
2025-11-27 22:13:45 +0100

    etape3corrige

--- Tp4/tp4exo2.c
+++ Tp4/tp4exo2.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void menu_affichage() {
-
+void afficher_menu() {
     printf("=== BANQUE - DISTRIBUTEUR ===\n");
-
     printf("1 - Faire un retrait\n");
-
     printf("2 - Quitter\n");
-
 }
 
- 
-
-int demander_retrait() {
-
-    int saisie;
-
+int saisir_montant() {
+    int m;
     printf("Montant a retirer : ");
-
-    scanf("%d", &saisie);
-
-    return saisie;
-
+    scanf("%d",&m);
+    return m;
 }
 
-int montant_correct(int valeur) {
-    if ( (valeur >= 5) && (valeur <= 1000) && (valeur % 5 == 0) ) {
-        return 1;
+int montant_valide(int montant) {
+    if (montant < 5 || montant > 1000 || montant % 5 != 0) {
+        printf("Erreur : montant invalide.\n");
+        printf("Le montant doit etre entre 5 euros et 1000 euros, divisible par 5.\n");
+        return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int choix;
+    afficher_menu();
+    printf("Votre choix : ");
+    scanf("%d",&choix);
+
+    if (choix == 1) {
+        int m = saisir_montant();
+        montant_valide(m);
     }
 
     return 0;
-
 }
